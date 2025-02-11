e3d2724e20d8d947b11b145184758b7ae5e09154
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-02-11 18:26:04 +0900

    学籍番号の項目を追加

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'student_id' => $this->faker->unique()->numerify('#######'),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,8 +1,12 @@
 export interface User {
     id: number;
+    student_id: string;
     name: string;
     email: string;
     email_verified_at?: string;
+    is_admin: number;
+    created_at: string;
+    updated_at: string;
 }
 
 export type PageProps<
