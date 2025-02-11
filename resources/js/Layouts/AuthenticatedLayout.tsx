b00270e820b09ec74acc7209be147012307083aa
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-02-11 22:07:46 +0900

    ナビゲーションのリンクを更新

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -28,10 +28,10 @@ export default function Authenticated({
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink
-                                    href={route("dashboard")}
-                                    active={route().current("dashboard")}
+                                    href={route("menu")}
+                                    active={route().current("menu")}
                                 >
-                                    Dashboard
+                                    メニュー
                                 </NavLink>
                             </div>
                         </div>
