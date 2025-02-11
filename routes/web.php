59ef849a32d11c2f133c892a399472e8e1ab478f
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-02-11 15:04:22 +0900

    トップとランディングページのファイルを作成

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+Route::get('/', function() {
+    return Inertia::render('AuthenticatedHome');
+})->middleware("auth");
+
+Route::get('/', function() {
+    return Inertia::render('Landing');
+});
+
+Route::get('/welcome', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
