8821718071d0bb0657e462b176a6c5d63961f286
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-02-12 23:23:21 +0900

    add BandController

---
+++ app/Http/Controllers/BandController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Band;
+use Illuminate\Http\Request;
+
+class BandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $band = Band::create([
+            'name' => $request->name,
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+    
+        $band = Band::findOrFail($id);
+    
+        $band->name = $request->name;
+        $band->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $band = Band::findOrFail($id);
+        
+        $band -> delete();
+    }
+}
