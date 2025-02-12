6c31a9f19d2a59163693ea94062a32504382b2d6
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-02-12 09:58:09 +0900

    ユーザーに認証されているかの項目を追加

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,8 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'is_admin' => 0,
+            'admin' => 0,
+            'verified' => 1,
             'remember_token' => Str::random(10),
         ];
     }
