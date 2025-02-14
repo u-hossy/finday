16ec78740ec0d7dec7a383136352b19fe971381b
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-02-14 14:47:31 +0900

    FIx import

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
