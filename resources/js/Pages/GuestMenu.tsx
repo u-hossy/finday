1a6dc66f515b4c9d29685e44dc5dbbd88ae5cd35
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-08-15 23:13:04 +0900

    WIP レイアウトを整えている

--- resources/js/Pages/GuestMenu.tsx
+++ resources/js/Pages/GuestMenu.tsx
@@ -1,10 +1,15 @@
 import MenuLayout from "@/Layouts/MenuLayout";
+import { Link } from "@inertiajs/react";
 
 export default function GuestMenu() {
     return (
         <>
             <MenuLayout>
-                <p>aaa</p>
+                <div className="flex flex-col items-center">
+                    <Link href="/dashboard" as="button">
+                        ログイン
+                    </Link>
+                </div>
             </MenuLayout>
         </>
     );
